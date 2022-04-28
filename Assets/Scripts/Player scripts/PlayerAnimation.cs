1a7c828bf8527f2e05567cfde2b4e78d1187d4c7
LazyP2018 <lazyp2018@gmail.com>
2022-04-28 12:46:24 +0200

    0.0.2
    Added player movement. Started making the map and bushes.

---
+++ Assets/Scripts/Player scripts/PlayerAnimation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] animSprites;
+    private float timeTreshold = 0.1f;
+    private float timer;
+    private int state = 0;
+
+    private SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timer)
+        {
+            sr.sprite = animSprites[state % animSprites.Length];
+            state++;
+            timer = Time.time + timeTreshold;
+        }
+    }
+}
---
+++ Assets/Scripts/Player scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 3f;
+
+    private Rigidbody2D myBody;
+    private Vector2 moveVector;
+    private SpriteRenderer sr;
+    private Animator anim;
+    private float harvestTimer;
+    private bool isHarvesting;
+    private GameObject artifact;
+    private string MOVEMENT_AXIS_X = "Horizontal";
+    private string MOVEMENT_AXIS_Y = "Vertical";
+    private string WALK_ANIMATION = "Walking";
+
+    
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        anim.SetBool(WALK_ANIMATION, false);
+    }
+
+    private void Update()
+    {
+        FlipSprite();
+        AnimatePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isHarvesting)
+        {
+            myBody.velocity = Vector2.zero;
+        }
+        else
+        {
+            moveVector = new Vector2(Input.GetAxis(MOVEMENT_AXIS_X), Input.GetAxis(MOVEMENT_AXIS_Y));
+            if (moveVector.sqrMagnitude > 1)
+                moveVector = moveVector.normalized;
+            myBody.velocity = new Vector2(moveVector.x * movementSpeed, moveVector.y * movementSpeed);
+        }
+    }
+    
+    private void FlipSprite()
+    {
+        if (Input.GetAxisRaw(MOVEMENT_AXIS_X) == 1)
+        {
+            sr.flipX = false;
+        } 
+        else if (Input.GetAxisRaw(MOVEMENT_AXIS_X) == -1)
+        { 
+            sr.flipX = true;
+        }
+    }
+
+    private void AnimatePlayer()
+    {
+        if (Input.GetAxisRaw(MOVEMENT_AXIS_X) > 0 || Input.GetAxisRaw(MOVEMENT_AXIS_X) < 0 || Input.GetAxisRaw(MOVEMENT_AXIS_Y) > 0 || Input.GetAxisRaw(MOVEMENT_AXIS_Y) < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+}
