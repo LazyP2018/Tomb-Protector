73cd07e308468bde7652372bedb1564558de61a8
LazyP2018 <lazyp2018@gmail.com>
2022-04-30 00:51:21 +0200

    0.0.8
    Added attacking the bush and tomb wolf AI and added more bushes

--- Assets/Scripts/Enemy scripts/WolfAI.cs
+++ Assets/Scripts/Enemy scripts/WolfAI.cs
@@ -46,10 +46,91 @@ public class WolfAI : MonoBehaviour
         tomb = GameObject.FindWithTag("Tomb").GetComponent<TombScript>();
     }
 
+    private void Update()
+    {
+        if (!tomb)
+            return;
+
+        if (isEater)
+        {
+            if (bushFruitsTarget && bushFruitsTarget.enabled && bushFruitsTarget.HasFruits() && !killingBush)
+            {
+                //walk to bush if not close enough
+                if (Vector2.Distance(transform.position, bushFruitsTarget.transform.position) > 0.5f)
+                {
+                    float step = moveSpeed * Time.deltaTime;
+                    transform.position = Vector2.MoveTowards(transform.position,
+                        bushFruitsTarget.transform.position, step);
+                    isMoving = true;
+                }
+                else
+                {
+                    isMoving = false;
+                    bushFruitsTarget.HarvestFruit();
+                    eatTimer = Time.time + eatTimeTreshold;
+                    killingBush = true;
+                }
+            }
+            else if (killingBush)
+            {
+                if (Time.time > eatTimer)
+                {
+                    bushFruitsTarget.EatBushFruits();
+                    killingBush = false;
+                    SearchForTarget();
+                }
+            }
+            else
+            {
+                SearchForTarget();
+            }
+
+            if (bushFruitsTarget)
+            {
+                if (bushFruitsTarget.transform.position.x < transform.position.x)
+                    left = true;
+                else
+                    left = false;
+            }
+
+            if (!bushFruitsTarget)
+                SearchForTarget();
+
+        }
+        else
+        {
+            if (Vector2.Distance(transform.position, tomb.transform.position) > 1.5f)
+            {
+                float step = moveSpeed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, tomb.transform.position, step);
+                isMoving = true;
+            }
+            else if (!isAttacking)
+            {
+                isAttacking = true;
+                attackTimer = Time.time + attackTimeTreshold;
+                isMoving = false;
+            }
+            if (isAttacking)
+            {
+                if (Time.time > attackTimer)
+                {
+                    Attack();
+                    attackTimer = Time.time + attackTimeTreshold;
+                }
+            }
+            if (tomb.transform.position.x < transform.position.x)
+                left = true;
+            else
+                left = false;
+        }
+    }
+
     void SearchForTarget()
     {
         bushFruitsTarget = null;
         Collider2D[] hits;
+        isMoving = false;
 
         for (int i = 1; i < 50; i++)
         {
